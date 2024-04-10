ed9f1b1fc8222e6e8de456610a3f550a792652d4
ymski <114902604+ymski@users.noreply.github.com>
2024-04-10 12:01:40 +0900

    add trace point
    Signed-off-by: ymski <114902604+ymski@users.noreply.github.com>

--- rcl/src/rcl/publisher.c
+++ rcl/src/rcl/publisher.c
@@ -263,6 +263,7 @@ rcl_publish_serialized_message(
     return RCL_RET_PUBLISHER_INVALID;  // error already set
   }
   RCL_CHECK_ARGUMENT_FOR_NULL(serialized_message, RCL_RET_INVALID_ARGUMENT);
+  TRACEPOINT(rcl_publish, (const void *)publisher, (const void *)serialized_message);
   rmw_ret_t ret = rmw_publish_serialized_message(
     publisher->impl->rmw_handle, serialized_message, allocation);
   if (ret != RMW_RET_OK) {
--- rcl/src/rcl/subscription.c
+++ rcl/src/rcl/subscription.c
@@ -602,6 +602,7 @@ rcl_take_serialized_message(
   }
   RCUTILS_LOG_DEBUG_NAMED(
     ROS_PACKAGE_NAME, "Subscription serialized take succeeded: %s", taken ? "true" : "false");
+  TRACEPOINT(rcl_take, (const void *)serialized_message);
   if (!taken) {
     return RCL_RET_SUBSCRIPTION_TAKE_FAILED;
   }
